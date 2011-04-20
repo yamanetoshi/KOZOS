b3f9356f73901f62881d0e0de1f81ee3f721efe5
yamanetoshi <yamanetoshi@gmail.com>
2011-04-20 10:11:41 +0900

    4th step

--- bootload/lib.c
+++ bootload/lib.c
@@ -80,12 +80,35 @@ int putc(unsigned char c)
   return serial_send_byte(SERIAL_DEFAULT_DEVICE, c);
 }
 
+/* １文字受信 */
+unsigned char getc(void)
+{
+  unsigned char c = serial_recv_byte(SERIAL_DEFAULT_DEVICE);
+  c = (c == '\r') ? '\n' : c;
+  putc(c); /* エコー・バック */
+  return c;
+}
+
 /* 文字列送信 */
 int puts(unsigned char *str)
 {
   while (*str)
     putc(*(str++));
   return 0;
+}
+
+/* 文字列受信 */
+int gets(unsigned char *buf)
+{
+  int i = 0;
+  unsigned char c;
+  do {
+    c = getc();
+    if (c == '\n')
+      c = '\0';
+    buf[i++] = c;
+  } while (c);
+  return i - 1;
 }
 
 /* 数値の16進表示 */
